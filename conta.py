85c9581f3e239db0f501c805e44e6c54d0d6fa93
pedrodjavan <pedrolucasnunesferreira36@gmail.com>
2026-05-20 16:50:53 -0300

    fiz a classe.

--- conta.py
+++ conta.py
@@ -1,1 +1,35 @@
+class Conta():
+  def __init__(self, saldo):
+    self.saldo = saldo
+
+  def saca(self, valor):
+    if valor <= self.saldo:
+      self.saldo -= valor
+      print(f"Saque de R${valor:.2f} realizado.")
+    else:
+      print("Saldo insuficiente.")
+
+      
+  def deposita(self, valor):
+      self.saldo += valor
+      print(f"Deposito de R${valor:.2f} realizado.")
+
+  def rendimento(self, valor):
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
